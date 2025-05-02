f5c5ba52d5ade66bc22e60eda1c446e59c2390db
Shivam Ray <shivamray4292@gmail.com>
2025-05-02 09:21:44 +0530

    JWT added

--- backend/app/auth/routes.py
+++ backend/app/auth/routes.py
@@ -5,11 +5,13 @@ from app.database import SessionLocal
 from passlib.context import CryptContext 
 from jose import JWTError, jwt 
 from datetime import datetime, timedelta
+from fastapi.security import OAuth2PasswordBearer
 
 SECRET_KEY = "your-secret-key" 
 ALGORITHM = "HS256" 
 ACCESS_TOKEN_EXPIRE_MINUTES = 30 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+Oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
 
 router = APIRouter()
 
@@ -25,6 +27,25 @@ def create_access_token(data: dict, expires_delta: timedelta):
     expire = datetime.utcnow() + expires_delta 
     to_encode.update({"exp": expire}) 
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+def get_current_user(token: str = Depends(Oauth2_scheme), db: Session = Depends(get_db)):
+    credentials_exception = HTTPException(
+        status_code=401,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+    user = crud.get_user_by_username(db, username=username)
+    if user is None:
+        raise credentials_exception
+    return user
+
 
 @router.post("/signup") 
 def signup(user: schemas.UserCreate, db: Session = Depends(get_db)):
--- backend/app/routes/tasks.py
+++ backend/app/routes/tasks.py
@@ -1,17 +1,21 @@
-from fastapi import APIRouter, Depends, HTTPException 
-from sqlalchemy.orm import Session 
-from app import schemas, crud, models 
-from app.auth.routes import get_db
+from fastapi import Depends, APIRouter, HTTPException
+from sqlalchemy.orm import Session
+from app import schemas, crud
+from app.database import SessionLocal
+from app.auth.routes import get_current_user, get_db
+from app.models import User
 
 router = APIRouter()
 
-#For simplicity, user ID is passed directly (ideally it should come from JWT)
+@router.get("/")
+def read_tasks(db: Session = Depends(get_db), 
+               current_user: User = Depends(get_current_user)):
+    return crud.get_tasks(db, user_id=current_user.id)
 
-@router.get("/") 
-def read_tasks(user_id: int, db: Session = Depends(get_db)): 
-    return crud.get_tasks(db, user_id)
+@router.post("/")
+def create_task(task: schemas.TaskCreate, 
+                db: Session = Depends(get_db), 
+                current_user: User = Depends(get_current_user)):
+    return crud.create_task(db, task, user_id=current_user.id)
 
-@router.post("/") 
-def create_task(task: schemas.TaskCreate, user_id: int, db: Session = Depends(get_db)): 
-    return crud.create_task(db, task, user_id)
 
